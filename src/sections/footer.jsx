f5cc20df23f63e0fc2b63c88cc1d9f4ae026ad8f
Piotr Kałuża <kontakt@piotrkaluza.pl>
2023-02-04 18:36:56 +0100

    Footer init & Social block

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -5,9 +5,10 @@ import "../scss/sections/footer.scss"
 
 export default function Footer() {
   return (
-    <div>
+    <div className="page-footer">
         <Contact />
         <Social />
+      <p className="page-footer__copy">&copy; Piotr Kałuża {(new Date()).getFullYear()}. All rights reserved.</p>
     </div>
   )
 }
--- src/sections/social.jsx
+++ src/sections/social.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import "../scss/sections/social.scss"
+import { SocialIcon } from 'react-social-icons';
 
 export default function Social() {
+  const socialLinks = [
+    'https://www.linkedin.com/in/peterkaluza/',
+    'https://github.com/Bush95',
+    'https://piotrkaluza.pl/',
+    'https://www.facebook.com/PiotrKaluzaFotografia'
+  ];
+
+  const socialLinkNodes = socialLinks.map((el, index) => <li key={index}><SocialIcon url={el} target="_blank" bgColor="#FFF" style={{ height: 45, width: 45 }} /></li>);
+
   return (
-    <div>Social</div>
+    <ul className="social">
+        {socialLinkNodes}
+    </ul>
   )
 }
